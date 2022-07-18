f70fc696c64e489d0659ad2103e141695b27fddb
Patobeur <patobeur41@gmail.com>
2022-07-18 02:07:37 +0200

    Fresh instance correction

--- js/mobs/Mobs.js
+++ js/mobs/Mobs.js
@@ -3,31 +3,34 @@ class Mobs {
 	#CurrentMobImmat
 	#Formula
 	constructor() {
-		this.mobConf = new MobConfig()
+
 		this.#AllMobs = []
 		this.#CurrentMobImmat = 0
 		this.#Formula = new Formula()
 	}
 
 	addOne(nickname = false, mobType = 'mobs') {
+
+		this.mobConf = new MobConfig()
+
+		// i get a clone of the default config
 		let conf = this.mobConf.get_(mobType)
 
-		// adding basics datas to conf
+		// adding basics to feet the needs
 		conf.immat = this.#CurrentMobImmat
 		conf.id = 'M_' + conf.immat
-
 		conf.position = this.#Formula.get_aleaPosOnScreen(conf.divs.prima.size)
 		conf.nickname = (!nickname === false) ? nickname : new String('UnNamed_') + conf.immat;
 		conf.theta.cur = this.#Formula.rand(0, 360)
 
-		let newmob = new Mob(
+		// push a fresh mob with fresh conf to allMob arrray
+		this.#AllMobs.push(new Mob(
 			conf
-		)
+		))
 
-		this.#AllMobs.push(newmob)
+		// set the new immat
 		this.#CurrentMobImmat = this.#AllMobs.length
 	}
-
 	// -------------------------------------------------------------
 	get_allMobs() {
 		return this.#AllMobs.length > 0 ? this.#AllMobs : false;
