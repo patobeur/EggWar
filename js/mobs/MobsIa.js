8b0df43ab7d275e3ed2ddabd19ba2400315492f4
Patobeur <patobeur41@gmail.com>
2022-07-18 12:32:05 +0200

    ia action is back

--- js/mobs/MobsIa.js
+++ js/mobs/MobsIa.js
@@ -3,15 +3,41 @@ class MobsIa {
 		this.Formula = new Formula()
 	}
 	iaAction(conf) {
+		// if (conf.ia.changeAction.cur === 0) {
+		// 	this.#chooseDir(conf)
+		// }
+
+
 		if (conf.ia.changeAction.cur === 0) {
-			this.#chooseDir(conf)
+
+			conf.ia.changeAction.lastAction = conf.ia.changeAction.currentAction
+
+			// new random action
+			let randDir = this.Formula.rand(0, 4)
+
+			// console.log(conf.ia.changeAction.currentAction)
+			switch (randDir) {
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+					break;
+				case 4:
+					this.#chooseDir(conf)
+					break;
+				default:
+					break;
+			}
+		}
+		else {
+			this.#keepMoving(conf)
 		}
 
+		// set current tics to min if current is bigger than max
 		conf.ia.changeAction.cur = conf.ia.changeAction.cur > conf.ia.changeAction.max
 			? 0
 			: conf.ia.changeAction.cur + 1
 
-		this.#keepMoving(conf)
 	}
 	#chooseDir(conf) {
 		let dir = this.Formula.rand(0, 1) > .5 ? 1 : -1;
